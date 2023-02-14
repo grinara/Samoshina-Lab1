85b6e269b666b2c05f82e51d40311616f031e3e5
серёжа белов <rtif1969@outlook.com>
2023-02-14 21:01:02 +0700

    Добавьте файлы проекта.

---
+++ Sourse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Sourse
+{
+     class Program
+    {
+        static void Main()
+        {
+            Console.Write("число - ");
+            var num = Int64.Parse(Console.ReadLine());
+            Console.Write("степень - ");
+            var step = Int64.Parse(Console.ReadLine());
+            int s = 1, s1 = 0;
+            for(int i = 1; i <= step; i++)
+            {
+                for(int j = 1; j <= num; j++)
+                {
+                    s1 = s1 + s;
+                }
+                s = s1;
+                s1 = 0;
+            }
+            Console.Write(s);
+        }
+    }
+}
