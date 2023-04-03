d4d407c336d35996e3adee193ada296a294e32a8
grinara <114216399+grinara@users.noreply.github.com>
2023-04-03 21:06:55 +0700

    Update Program.cs

--- Sourse/Program.cs
+++ Sourse/Program.cs
@@ -1,25 +1,32 @@
-﻿using System;
-namespace Sourse
+using System;
+namespace Samohina_Lab1
 {
-     class Program
+    class Step
     {
-        static void Main()
-        {
-            Console.Write("число - ");
-            var num = Int64.Parse(Console.ReadLine());
-            Console.Write("степень - ");
-            var step = Int64.Parse(Console.ReadLine());
-            int s = 1, s1 = 0;
-            for(int i = 1; i <= step; i++)
+        static float Deg(float num, float step) {
+            if (step == 0 || num == 1) { return 1; }
+            if (num == 0) {  return 0; }
+            float s = 1, s1 = 0;
+            for (int i = 1; i <= Math.Abs(step); i++)
             {
-                for(int j = 1; j <= num; j++)
+                for (int j = 1; j <= Math.Abs(num); j++)
                 {
                     s1 = s1 + s;
                 }
                 s = s1;
                 s1 = 0;
             }
-            Console.Write(s);
+            if (num < 0 && step%2!=0) { s*=-1; }
+            if(step<0) { s=1/s; }
+            return s;
+        }
+        static void Main()
+        {
+            Console.Write("число - ");
+            var num = float.Parse(Console.ReadLine());
+            Console.Write("степень - ");
+            var step = float.Parse(Console.ReadLine());
+            Console.Write(Deg(num, step));
         }
     }
 }
